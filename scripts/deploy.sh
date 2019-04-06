cd413c2241d16dd08de6d5b749b3ae394e2e6f65
Édouard Denommée <moronicproduct3@gmail.com>
2019-04-05 20:54:44 -0400

    deploy script planning

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# This script is meant to deploy all config files onto a new machine
+
+
+# Create required directory structure
+
+# Check if git is installed and executable
+# Check if it can be installed
+    # Install git
+    # Clone settings
+# Or download settings using curl
+
+# backup old config files
+
+# generate new dotfiles sourcing the ones in the repo
